6026c8b01db2ac10df478d49c70bf15828f93006
Jerrext <113127858+Jerrext@users.noreply.github.com>
2023-02-10 07:49:47 +0300

    Add Button, UserName and Title components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import { ButtonType } from './components/Button/Button';
+import UserName from './components/UserName';
+import Title from './components/Title';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button title={"Primary"} type={ButtonType.Primary} onClick={() => {}} />
+      <Button title={"Secondary"} type={ButtonType.Secondary} onClick={() => {}} />
+      <Button title={"Error"} type={ButtonType.Error} onClick={() => {}} />
+      <UserName userName='Artem Malkin' />
+      <Title />
     </div>
   );
 }
